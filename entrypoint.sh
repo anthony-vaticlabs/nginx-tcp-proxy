5a5b856b8a5a150b88bb2475719c782f609f91a9
Anthony Chiang <anthony@vaticlabs.ai>
2022-08-25 17:15:00 +0000

    Support configurable nginx worker connections

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
-sed -i -e s/__HOST__/$HOST/g -e s/__PORT__/$PORT/g /etc/nginx/nginx.conf
+WORKERS=${WORKERS:=1024}
+sed -i -e s/__HOST__/$HOST/g -e s/__PORT__/$PORT/g -e s/__WORKERS__/$WORKERS/g /etc/nginx/nginx.conf
 nginx -g "daemon off;"
